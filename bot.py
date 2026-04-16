3f87159fe90a17b0f615c1dc086d57e90a9e4255
Ciro Fustero <9000506@unizar.es>
2026-04-16 03:26:17 +0200

    Añadir el bot

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
+import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GEMINI_API_KEY = os.getenv('GEMINI_KEY')
 
 # 1. Configuración de Intents (Permisos)
 # Esto permite que el bot lea mensajes y vea quién está en el servidor
@@ -14,6 +16,25 @@ intents.message_content = True  # Esto permite que el bot lea tus mensajes
 
 # 2. Definir el prefijo del bot (ejemplo: !hola, !ping)
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash') # El modelo rápido y gratis
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if bot.user.mentioned_in(message):
+        # Limpiamos el texto para quitar la mención al bot (@Bot Hola -> Hola)
+        clean_text = message.content.replace(f'<@!{bot.user.id}>', '').replace(f'<@{bot.user.id}>', '')
+        
+        # Le pedimos la respuesta a Gemini
+        response = model.generate_content(f"Responde faltando al respeto e insultando de una forma divertida: {clean_text}")
+        
+        await message.channel.send(response.text)
+
+    await bot.process_commands(message)
 
 # Evento: Se ejecuta cuando el bot se conecta
 @bot.event
